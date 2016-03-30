3c9ab43eff4d4866372f8b4785d410837d588c0f
Tom  Christian <tctc91@gmail.com>
2016-03-30 22:48:23 +0100

    post content from api now compiles + moment integration

--- src/posts/components/post.component.ts
+++ src/posts/components/post.component.ts
@@ -1,16 +1,18 @@
-import {Component} from 'angular2/core';
+import {Component, DynamicComponentLoader, ElementRef} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import {Router, RouteParams} from 'angular2/router';
 
 import {PostService} from '../../shared/services/postService';
 import {TagService} from '../../shared/services/tagService';
 
+declare var moment: any;
+
 @Component({
   selector: 'app-post',
   moduleId: module.id,
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
-  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
+  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class PostComponent {
 	slug: string;
@@ -18,15 +20,21 @@ export class PostComponent {
 	gotPost: boolean = false;
 	goPrev: any;
 	goNext: any;
+	directiveTest: boolean;
 
-	constructor(private _routeParams: RouteParams,
+	constructor(
+		private dcl: DynamicComponentLoader,
+		private elementRef: ElementRef,
+		private _routeParams: RouteParams,
 		private _router: Router,
+		private _dcl: DynamicComponentLoader,
 		private _postService: PostService,
 		private _tagService: TagService) {
 		this.slug = _routeParams.get('slug');
 
 		this._postService.fetchSinglePost(this.slug).subscribe((e) => {
 			this.post = this._postService.postSingle;
+			this.post.date = moment(this.post.date).format('MMMM Do YYYY');
 			this.gotPost = true;
 
 			this._tagService.fetchTagsCollection(this.post.tags).subscribe((tags) => {
@@ -36,7 +44,26 @@ export class PostComponent {
 			this._postService.fetchAllPosts().subscribe(() => {
 				this.setupPagination(this._postService.postCollection);
 			});
+
+			let template = `<hello></hello><h2>${this.post.content.rendered}</h2>`;
+			let directives = [FORM_DIRECTIVES, CORE_DIRECTIVES];
+			this.dcl.loadIntoLocation(this.buildComponent(template, directives), this.elementRef, 'postContent');
 		});
+	}
+
+	/* Hacky way to do the equivalent of NG1's $compile (http://stackoverflow.com/a/34786039/1010691) */
+	buildComponent(template: any, directives: any) {
+		@Component({
+			selector: 'fake',
+			template, directives
+		})
+		class FakeComponent {
+			directiveTest: boolean;
+			ngOnInit() {
+				this.directiveTest = false;
+			}
+		};
+		return FakeComponent;
 	}
 
 	setupPagination(posts: any[]) {
