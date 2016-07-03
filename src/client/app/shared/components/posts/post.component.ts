642c831a530b76af97a5a5e8d2c475973c2a1f43
Tom  Christian <tctc91@gmail.com>
2016-07-03 21:20:15 +0100

    post content rendering

--- src/client/app/shared/components/posts/post.component.ts
+++ src/client/app/shared/components/posts/post.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CORE_DIRECTIVES } from '@angular/common';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute } from '@angular/router';
 
-// import {MediaComponent} from '../media/media.component';
 import { PostService } from '../../services/post.service';
 import { TagService } from '../../services/tag.service';
 import { EventStore } from '../../misc/eventStore';
@@ -24,23 +23,16 @@ export class PostComponent {
 	gotPost: boolean = false;
 	goPrev: any;
 	goNext: any;
-	directiveTest: boolean;
 
 	constructor(
-		//private dcl: DynamicComponentLoader,
-		//private elementRef: ElementRef,
-		// private _routeParams: RouteParams,
 		private _route: ActivatedRoute,
 		private _router: Router,
-		//private _dcl: DynamicComponentLoader,
 		private _postService: PostService,
 		private _tagService: TagService,
 		private _eventStore: EventStore) {
 	}
 
 	ngOnInit() {
-		this.postReady = false;
-
 		this.nav = this._route.params.subscribe(params => {
 			this.slug = params['slug'];
 
@@ -48,44 +40,15 @@ export class PostComponent {
 				this.post = this._postService.postSingle;
 				this.post.date = moment(this.post.date).format('MMMM Do YYYY');
 
-				this._tagService.fetchTagsCollection(this.post.tags).subscribe((tags) => {
-					this.post.tags = tags;
-				});
-
 				this.setupPagination(this._postService.postCollection);
 
-				this.postReady = true;
-
-				// let template = `${this.post.content.rendered}`;
-				// let directives = [CORE_DIRECTIVES, MediaComponent];
-				// this.dcl.loadIntoLocation(this.buildComponent(template, directives, this.post.id), this.elementRef, 'postContent');
+				this._tagService.fetchTagsCollection(this.post.tags).subscribe((tags) => {
+					this.post.tags = tags;
+					this.postReady = true;
+				});
 			});
 		});
-
-		this._eventStore.postContentReady.subscribe((id: any) => {
-			if (this.post.id === id) {
-				// @todo The component instance isn't being unloaded for some reason.
-				this.postReady = true;
-			}
-		});
 	}
-
-	/* Hacky way to do the equivalent of NG1's $compile (http://stackoverflow.com/a/34786039/1010691) */
-	// buildComponent(template: any, directives: any, postId: number) {
-	// 	@Component({ selector: 'fake', template, directives })
-	// 	class FakeComponent {
-	// 		directiveTest: boolean;
-	// 		constructor(private _eventStore: EventStore) {}
-	// 		ngAfterViewInit() {
-	// 			this.directiveTest = false;
-	// 			this._eventStore.postContentReady.emit(postId);
-	// 		}
-	// 		ngOnDestroy() {
-	// 			postId = null;
-	// 		}
-	// 	};
-	// 	return FakeComponent;
-	// }
 
 	setupPagination(posts: any[]) {
 		let self = this;
