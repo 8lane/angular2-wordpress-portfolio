19a239fa5cda4b4f3f1011247700cd4c477b33ce
Tom  Christian <tctc91@gmail.com>
2016-12-11 19:24:57 +0000

    plunker urls updated

--- src/client/app/+sidebar/sidebarPosts.ts
+++ src/client/app/+sidebar/sidebarPosts.ts
@@ -5,9 +5,9 @@ export class SidebarPosts {
   static list = [
 		{Name: 'Color palettes in SASS', Url: 'https://medium.com/p/dc0b13d9ae74/', Source: 'medium', Featured: true},
     {Name: 'angular2-wordpress-portfolio', Url: 'https://github.com/tctc91/angular2-wordpress-portfolio', Source: 'github', Featured: true},
-    {Name: 'Undo delete design pattern', Url: 'https://codepen.io/tctc91/pen/mOBoEp', Source: 'codepen', Featured: false},
+    {Name: 'Undo delete design pattern', Url: 'https://embed.plnkr.co/3QT7JQ/', Source: 'codepen', Featured: false},
     {Name: 'Media query mixin in SASS', Url: 'https://codepen.io/tctc91/pen/ZBXPOz', Source: 'codepen', Featured: false},
     {Name: 'angular-holiday-chooser', Url: 'https://github.com/tctc91/angular-holiday-chooser', Source: 'github', Featured: false},
-    {Name: 'Form loading button animation', Url: 'https://codepen.io/tctc91/pen/pNWYgm', Source: 'codepen', Featured: false}
+    {Name: 'Form loading button animation', Url: 'https://embed.plnkr.co/bp0CMY/', Source: 'codepen', Featured: false}
   ];
 }
