a8ae264a01d6d4bc32824211c0536da68df0b5fd
Tom  Christian <tctc91@gmail.com>
2017-03-07 21:48:51 +0000

    sidebar updated

--- src/client/app/+sidebar/sidebarPosts.ts
+++ src/client/app/+sidebar/sidebarPosts.ts
@@ -3,11 +3,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class SidebarPosts {
   static list = [
+    {Name: 'Live Departures in React', Url: 'https://github.com/tctc91/react-mystop', Source: 'github', Featured: false},
 		{Name: 'Color palettes in SASS', Url: 'https://medium.com/p/dc0b13d9ae74/', Source: 'medium', Featured: true},
-    {Name: 'angular2-wordpress-portfolio', Url: 'https://github.com/tctc91/angular2-wordpress-portfolio', Source: 'github', Featured: true},
-    {Name: 'Undo delete design pattern', Url: 'https://embed.plnkr.co/3QT7JQ/', Source: 'codepen', Featured: false},
-    {Name: 'Media query mixin in SASS', Url: 'https://codepen.io/tctc91/pen/ZBXPOz', Source: 'codepen', Featured: false},
-    {Name: 'angular-holiday-chooser', Url: 'https://github.com/tctc91/angular-holiday-chooser', Source: 'github', Featured: false},
-    {Name: 'Form loading button animation', Url: 'https://embed.plnkr.co/bp0CMY/', Source: 'codepen', Featured: false}
+    {Name: 'Angular2 / Wordpress Portfolio', Url: 'https://github.com/tctc91/angular2-wordpress-portfolio', Source: 'github', Featured: true},
+    {Name: 'Node IPS Credit Scraper', Url: 'https://github.com/tctc91/nodejs-invision-credit-scraper', Source: 'github', Featured: false},
+    {Name: 'SASS Media query mixin', Url: 'https://codepen.io/tctc91/pen/ZBXPOz', Source: 'codepen', Featured: false},
+    {Name: 'Holiday Chooser in Angular', Url: 'https://github.com/tctc91/angular-holiday-chooser', Source: 'github', Featured: false},
   ];
 }
